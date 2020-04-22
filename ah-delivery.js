9ca41a46e02b5f025b5873ddb78ee4eb195fe02a
Jarflux <ben.oeyen@gmail.com>
2020-04-22 21:32:15 +0200

    Update ah-delivery.js

--- ah-delivery.js
+++ ah-delivery.js
@@ -1,17 +1,24 @@
+var apiUrl = 'https://www.ah.be/service/rest/delegate?url=%2Fkies-moment%2Fophalen%2F3224&_=1587583456543'; 
+var afterDate = new Date('2020-04-24');
+var beforeDate = new Date('2020-04-29');
 console.log("Checking Available AH Delivery Timeslots");
-var audio = new Audio("https://soundbible.com/mp3/Slow_HeartBeat-Mike_Koenig-1853475164.mp3");
+var audio = new Audio('https://soundbible.com/mp3/Slow_HeartBeat-Mike_Koenig-1853475164.mp3');
 window.intervalID = setInterval( function(){
     var allFull = true;
-    $.getJSON('https://www.ah.be/service/rest/delegate?url=%2Fkies-moment%2Fophalen%2F3224&_=1586081256141', function(data) {
+    $.getJSON(apiUrl, function(data) {
        var deliveryDates = data._embedded.lanes[3]._embedded.items[0]._embedded.deliveryDates;
        deliveryDates.forEach( function(deliverydate){
-           deliverydate.deliveryTimeSlots.forEach(function(timeslot){
-               if(timeslot.state=="selectable"){
-                   allFull = false;
-                   console.log("%cAvailable slot found ->  %s at %s", "color: #228B22",  deliverydate.date, timeslot.from);
-                   audio.play();
-               }
-           })
+           if((new Date(deliverydate.date)).getTime() > afterDate.getTime() 
+                && (new Date(deliverydate.date)).getTime() < beforeDate.getTime() ){
+                console.log("Checking date " + deliverydate.date);
+                deliverydate.deliveryTimeSlots.forEach(function(timeslot){
+                    if(timeslot.state=="selectable" ){
+                        allFull = false;
+                        console.log("%cAvailable slot found ->  %s at %s", "color: #228B22",  deliverydate.date, timeslot.from);
+                        audio.play();
+                    }
+                })
+            }
        })
        if(allFull){
            console.log("%cAll timeslots full", "color: #D3D3D3");
